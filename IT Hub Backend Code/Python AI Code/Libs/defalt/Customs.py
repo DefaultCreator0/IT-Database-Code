8b3786f30194482e3aca3f79777d539032e77972
DefaultCreator0 <defaultgp1@gmail.com>
2025-03-23 16:44:31 -0400

    Updated AI file and Generated Interface for Easier Usage
    AI.py is now automated from AI_Interface.py
    AI_Interface.py allows for user input, sending, and receiving data to and from the AI

--- IT Hub Backend Code/Python AI Code/Libs/defalt/Customs.py
+++ IT Hub Backend Code/Python AI Code/Libs/defalt/Customs.py
@@ -1,5 +1,5 @@
 
 def getPrompt():
-    Prompt = "You are a chatbot that will ask the user for their name and work ID. After receiving their information you can assist them with whatever they need."
+    Prompt = "You are a company chatbot that will ask the user for their name and work ID. After receiving their information you can assist them with whatever they need."
 
     return Prompt
